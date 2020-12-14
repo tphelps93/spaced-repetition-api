0d774692240d3e71e36ab105e1d13337cc8c2b75
Taylor Phelps <tphelps1993@gmail.com>
2020-12-14 12:03:05 -0500

    update

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -1,41 +1,83 @@
 BEGIN;
 
-TRUNCATE
-  "word",
-  "language",
-  "user";
+TRUNCATE "word",
+"language",
+"user";
 
-INSERT INTO "user" ("id", "username", "name", "password")
+INSERT INTO
+  "user" ("id", "username", "name", "password")
 VALUES
   (
     1,
-    'admin',
-    'Dunder Mifflin Admin',
+    'tphelps93',
+    'Taylor Phelps',
     -- password = "pass"
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   );
 
-INSERT INTO "language" ("id", "name", "user_id")
+INSERT INTO
+  "language" ("id", "name", "user_id")
 VALUES
-  (1, 'French', 1);
+  (1, 'German', 1);
 
-INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
+INSERT INTO
+  "word" (
+    "id",
+    "language_id",
+    "original",
+    "translation",
+    "next"
+  )
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'trainieren', 'practice', null),
+  (2, 1, 'hallo', 'hello', null),
+  (3, 1, 'maison', 'haus', null),
+  (4, 1, 'Entwickler', 'developer', null),
+  (5, 1, 'Übersetzen', 'translate', null),
+  (6, 1, 'tolle', 'amazing', null),
+  (7, 1, 'hund', 'dog', null),
+  (8, 1, 'katze', 'cat', null);
 
-UPDATE "language" SET head = 1 WHERE id = 1;
+UPDATE
+  "language"
+SET
+  head = 1
+WHERE
+  id = 1;
 
 -- because we explicitly set the id fields
 -- update the sequencer for future automatic id setting
-SELECT setval('word_id_seq', (SELECT MAX(id) from "word"));
-SELECT setval('language_id_seq', (SELECT MAX(id) from "language"));
-SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
+SELECT
+  setval(
+    'word_id_seq',
+    (
+      SELECT
+        MAX(id)
+      from
+        "word"
+    )
+  );
+
+SELECT
+  setval(
+    'language_id_seq',
+    (
+      SELECT
+        MAX(id)
+      from
+        "language"
+    )
+  );
+
+SELECT
+  setval(
+    'user_id_seq',
+    (
+      SELECT
+        MAX(id)
+      from
+        "user"
+    )
+  );
 
 COMMIT;
--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL:
     process.env.DB_URL ||
-    'postgresql://dunder-mifflin@localhost/spaced-repetition',
+    'postgresql://tphelps93@localhost/spaced-repetition',
   TEST_DB_URL: process.env.TEST_DB_URL,
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
