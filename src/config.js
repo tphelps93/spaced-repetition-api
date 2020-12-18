9b7aece9f3414d4d51ce39bdab9fca5189cb1db4
Taylor Phelps <tphelps1993@gmail.com>
2020-12-18 14:16:08 -0500

    updated config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
-    process.env.DATABASE_URL ||
+  process.env.DATABASE_URL + (process.env.NODE_ENV === 'production' ? '?ssl=true' : '') ||
     'postgresql://tphelps93@localhost/spaced-repetition',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
