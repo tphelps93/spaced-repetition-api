98b129ea82f98bd4b9589cbb1fbfb03eb1149812
Taylor Phelps <tphelps1993@gmail.com>
2020-12-18 14:12:44 -0500

    updated error handling

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,10 +1,8 @@
 const { NODE_ENV } = require('../config');
 
 module.exports = function errorHandler(error, req, res, next) {
-  const response =
-    NODE_ENV === 'production'
-      ? { error: 'Server error' }
-      : (console.error(error), { error: error.message, details: error });
+  const response = { error: 'Server error' };
+  console.error(error);
 
   res.status(500).json(response);
 };
