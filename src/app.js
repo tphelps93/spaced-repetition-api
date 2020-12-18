17f8e2a1fae003d59647676252d2fa66aa746aeb
Taylor Phelps <tphelps1993@gmail.com>
2020-12-18 12:05:35 -0500

    updated

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const errorHandler = require('./middleware/error-handler');
